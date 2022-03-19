67543f5e4b8e9fcfd1bbfb72c01e66758de34cd9
pavel.genuine <pavel.play@gmail.com>
2022-03-19 07:59:48 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,15 +1,48 @@
 import logo from './logo.svg';
 import './App.css';
-import Todos from './components/todos/todos';
 
 function App() {
   return (
     <div className="App">
-      <h1>Welcome to Todo Json Placeholder</h1>
-      <Todos></Todos>
-     
+
+      <Blog heading='Love Life' author='Mr.Jorder'  ></Blog>
+      <Blog heading='Love coding' author='Mr.charm'  ></Blog>
+      <Blog heading='Love starup' author='Mr.lovely'  ></Blog>
+      
     </div>
   );
+}
+
+function Blog(props){
+  return(
+    <div>
+      <h1>
+        Heading : {props.heading} , Author : {props.author}
+      </h1>
+      <article className='blog'>
+        <p style={{backgroundColor:'yellow', border:'3px solid red'}}>
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nostrum, rem.
+        </p>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Alias ex cum, ipsum exercitationem facilis a?
+      </article>
+    </div>
+  )
+}
+
+function Mobile(){
+
+
+  
+
+  return (
+    <div>
+      <h4>100</h4>
+      <button onClick={Battery} >Battery Down</button>
+    </div>
+  )
+
+
+
 }
 
 export default App;
